f65c112034ffcd329d5affdf13e9b78427cdd26e
vasishthroy <royvasishth99@gmail.com>
2025-02-02 14:03:55 +0530

    Created delete and Create functions for DELETE and POST methods

--- crudaction/crud-actions.go
+++ crudaction/crud-actions.go
@@ -3,8 +3,10 @@ package crudaction
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"simpleCrud/model"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -29,4 +31,30 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movies)
 }
