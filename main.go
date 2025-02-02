d318099c05f89a695ce52a449eff4174f11c6d99
vasishthroy <royvasishth99@gmail.com>
2025-02-02 12:33:36 +0530

    Created data-model package containing custom data type Movie and Director

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
+// "log"
 import (
 	"fmt"
+	"simpleCrud/model"
 )
 
+var movies []model.Movie
+
 func main() {
-	fmt.Print("Hello CRUD")
+
+	movies = append(
+		movies,
+		model.Movie{
+			ID:    "1",
+			Isbn:  "23401",
+			Title: "One Piece",
+			Director: &model.Director{
+				Firstname: "Luffy",
+				Lastname:  "Monkey D",
+			},
+		},
+	)
+
+	movies = append(
+		movies,
+		model.Movie{
+			ID:    "2",
+			Isbn:  "11111",
+			Title: "One Piece",
+			Director: &model.Director{
+				Firstname: "Natsu",
+				Lastname:  "Dragneel",
+			},
+		},
+	)
+
+	fmt.Printf("Starting server at port 8000\n")
+
 }
